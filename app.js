e1259666f28f87c6e03b6717e19a2a31ea277d42
aaditya010202 <aadibhatnagar535@gmail.com>
2023-06-18 23:19:24 +0530

    mongoDB added

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql").graphqlHTTP;
 const { buildSchema } = require("graphql");
-
+const mongoose = require("mongoose");
 const app = express();
-
+const Event = require("./models/event");
 app.use(bodyParser.json());
 
-const events = [];
 // "events" and "createEvents" are called resolvers
 // "!" will not return null string and also not null list of strings in RootQuery
 // rootvalue has the resolver function
@@ -46,17 +45,43 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find() //.find() will give all the events because we did not use any filters like title, description etc.
+          .then((events) => {
+            return events.map((event) => {
+              //events returned by mongoose has some metadata attached to it so we will use map function
+              return { ...event._doc, _id: event._doc._id.toString() }; //now this will give data without metadata
+              // the code works even if we remove _id: event._doc._id.toString() .... the function of this is to convert the id to string as it might not be returned in string type
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        // const event = {
+        //   _id: Math.random().toString(),
+        //   title: args.eventInput.title,
+        //   description: args.eventInput.description,
+        //   price: +args.eventInput.price, // + is to make sure that the input is converted to number
+        //   date: args.eventInput.date,
+        // };
+
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price, // + is to make sure that the input is converted to number
-          date: args.eventInput.date,
-        };
-        events.push(event);
+          date: new Date(args.eventInput.date),
+        });
+        return event //return to let know that this an async operation and you have to wait
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc, _id: event.id }; // instead of using  _id: event._doc._id.toString()  we can also use  _id: event.id  as .id is inbuilt in mongoose to return id in string
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
         return event;
       },
     },
@@ -64,4 +89,13 @@ app.use(
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.onkgtck.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
